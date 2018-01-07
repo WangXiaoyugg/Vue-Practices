1a3496b4b3b83faa0173c3cfcaae7833da86c732
GarenWang <2270099149@qq.com>
2018-01-07 20:51:30 +0800

    counter components

---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="counter">
+     <button @click="add">+</button>
+     <button @click="subtract">-</button>
+    <p>child:{{num}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'counter',
+    props:['num'],
+    data () {
+      return {
+      }
+    },
+    methods:{
+      add(){
+//        this.num++;
+        this.$emit('add');
+
+      },
+      subtract(){
+//        this.num--;
+        this.$emit('subtract');
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ counter/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Counter
+      v-bind:num="num"
+      v-on:add="add"
+      v-on:subtract="subtract"></Counter>
+    <p>parent:{{num}}</p>
+  </div>
+</template>
+
+<script>
+
+import Counter from './Counter'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      num:10,
+    }
+  },
+  components:{
+    Counter
+  },
+  methods:{
+    add(){
+      this.num++;
+    },
+    subtract(){
+      this.num--;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
